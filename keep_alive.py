580afc0b48beb459e97a21499cf6956a2d4d1d05
whatsapp110x-ctrl <whatsapp110x@gmail.com>
2025-08-08 18:54:49 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from threading import Thread
+import os
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "✅ Bot is running!"
 
 def run():
-    app.run(host='0.0.0.0', port=8000)
+    # Render requires using the PORT environment variable
+    port = int(os.getenv("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
